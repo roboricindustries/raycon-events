369712542b4fef9a4d321f8940bba65588f07ed9
Ilia <ilya.makarochkin@yandex.kz>
2025-10-08 11:47:58 +0500

    publisher update

--- pkg/pubsub/v2/publisher.go
+++ pkg/pubsub/v2/publisher.go
@@ -52,3 +52,51 @@ func (c *Client) PublishJSON(ctx context.Context, exchange, routingKey string, e
 		AppId:         FirstNonEmpty(c.config.Queues["producer"], ""), // or c.config.DefaultProducer
 	})
 }
+
+func (c *Client) NotifyChan(ctx context.Context, withChan func(ch *amqp.Channel, confirms <-chan amqp.Confirmation) error) error {
+	ch, err := c.pool.Borrow(ctx, c.config.PoolRetryDelayMs)
+	if err != nil {
+		return fmt.Errorf("borrow channel: %w", err)
+	}
+	defer c.pool.Return(ch)
+	if err := ch.Confirm(false); err != nil {
+		return err
+	}
+	confirms := ch.NotifyPublish(make(chan amqp.Confirmation, 1))
+	err = withChan(ch, confirms)
+	return err
+}
+
+func (c *Client) RawPublish(ctx context.Context, ch *amqp.Channel, exchange, routingKey string, env common.Envelope) error {
+	if exchange == "" {
+		exchange = c.config.DefaultDeliveryExchange
+	}
+	if routingKey == "" {
+		routingKey = c.config.DefaultDeliverRoutingKey
+	}
+
+	// Ensure metadata consistency
+	if env.Meta.ID == "" {
+		return fmt.Errorf("envelope.Meta.ID is required")
+	}
+	if env.Meta.CorrelationID == "" {
+		env.Meta.CorrelationID = env.Meta.ID // fallback to ID if no correlation
+	}
+	if env.Meta.Time.IsZero() {
+		env.Meta.Time = time.Now().UTC()
+	}
+	body, err := json.Marshal(env)
+	if err != nil {
+		return fmt.Errorf("marshal envelope: %w", err)
+	}
+	return ch.PublishWithContext(ctx, exchange, routingKey, false, false, amqp.Publishing{
+		ContentType:   "application/json",
+		Body:          body,
+		DeliveryMode:  amqp.Persistent,
+		MessageId:     env.Meta.ID,
+		CorrelationId: env.Meta.CorrelationID,
+		Type:          env.Meta.Type,
+		Timestamp:     env.Meta.Time,
+		AppId:         FirstNonEmpty(c.config.Queues["producer"], ""), // or c.config.DefaultProducer
+	})
+}
