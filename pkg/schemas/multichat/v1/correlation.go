2b9fd7ac358202c8794aaaa768ddf79e609906ab
Ilia <ilya.makarochkin@yandex.kz>
2025-10-09 14:10:36 +0500

    json annotations

--- pkg/schemas/multichat/v1/correlation.go
+++ pkg/schemas/multichat/v1/correlation.go
@@ -3,10 +3,10 @@ package multichat
 import "time"
 
 type ChatEgressCorrelationV1 struct {
-	Tenant       TenantRef
-	Provider     ProviderRef     // includes instance_id
-	Conversation ConversationKey // include provider_chat_id if you have it
-	OutboundID   string
-	Message      MessageKey // provider_message_id
-	AtProcessor  time.Time
+	Tenant       TenantRef       `json:"tenant"`
+	Provider     ProviderRef     `json:"provider"`
+	Conversation ConversationKey `json:"conversation"`
+	OutboundID   string          `json:"outbound_id"`
+	Message      MessageKey      `json:"message"`
+	AtProcessor  time.Time       `json:"at_processor"`
 }
