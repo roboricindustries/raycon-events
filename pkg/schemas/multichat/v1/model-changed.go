97d81605fa8efc7923684c7de34ce6c3770a2f84
bolatl <bolatlobakbai@gmail.com>
2026-03-17 11:48:35 +0500

    Added TenantRef, AggregateRef, KeyPart, and ModelChangedV2

--- pkg/schemas/multichat/v1/model-changed.go
+++ pkg/schemas/multichat/v1/model-changed.go
@@ -1,5 +1,7 @@
 package multichat
 
+import "encoding/json"
+
 type Aggregate struct {
 	Type    string `json:"type"`    // e.g. "deal" | "company" | "manager"
 	Version int64  `json:"version"` // monotonically increasing
@@ -11,4 +13,23 @@ type ModelChangedV1 struct {
 	Aggregate Aggregate      `json:"aggregate"`
 	Kind      string         `json:"kind"` // create|update|delete
 	Patch     map[string]any `json:"patch"`
+}
+
+type AggregateRef struct {
+	ID  *uint64   `json:"id,omitempty"`
+	Key []KeyPart `json:"key,omitempty"`
+}
+
+type KeyPart struct {
+	Name  string `json:"name"`
+	Value string `json:"value"`
+}
+
+type ModelChangedV2 struct {
+	Model        string          `json:"model"`
+	Op           string          `json:"op"` // operation
+	Version      uint64          `json:"version"`
+	AggregateRef AggregateRef    `json:"aggregate_ref"`
+	TenantRef    *TenantRef      `json:"tenant_ref,omitempty"`
+	Doc          json.RawMessage `json:"doc,omitempty"`
 }
